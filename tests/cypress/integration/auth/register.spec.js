3267837452786cdeb3a61b190d8c450c29bf6c68
Evandro Ribeiro <ribeiroevandro@live.com>
2024-04-15 06:30:15 -0300

    chore: remove failed test

--- tests/cypress/integration/auth/register.spec.js
+++ tests/cypress/integration/auth/register.spec.js
@@ -10,22 +10,4 @@ describe('Register page', () => {
         cy.url().should('be.equal', `${Cypress.config("baseUrl")}/dashboard`);
         cy.contains('You\'re logged in!');
     });
-
-    it('display message if email is already registered', () => {
-
-        cy.visit('/register');
-        cy.get('#name').type('Evandro Ribeiro');
-        cy.get('#email').type('ribeiroevandro@live.com');
-        cy.get('#password').type('SaVLAq3&vX6F!ik&k8kiA^bAyLPN$@io');
-        cy.get('#password_confirmation').type('SaVLAq3&vX6F!ik&k8kiA^bAyLPN$@io');
-        cy.get('[data-test-id="register_button"]').click()
-
-        cy.visit('/register');
-        cy.get('#name').type('Evandro Ribeiro');
-        cy.get('#email').type('ribeiroevandro@live.com');
-        cy.get('#password').type('SaVLAq3&vX6F!ik&k8kiA^bAyLPN$@io');
-        cy.get('#password_confirmation').type('SaVLAq3&vX6F!ik&k8kiA^bAyLPN$@io');
-        cy.get('[data-test-id="register_button"]').click()
-        cy.contains('The email has already been taken.');
-    });
 });
