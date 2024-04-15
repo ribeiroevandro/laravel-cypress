a9e1d90a227636cfa54f28a8f5b4e0416f4a2d46
Evandro Ribeiro <ribeiroevandro@live.com>
2024-04-15 04:30:51 -0300

    chore: update

---
+++ tests/cypress/integration/auth/register.spec.js
@@ -0,0 +1,31 @@
+describe('Register page', () => {
+    it('register a new user', () => {
+        cy.exec('php artisan migrate:fresh')
+        cy.visit('/register');
+        cy.get('#name').type('Evandro Ribeiro');
+        cy.get('#email').type('ribeiroevandro@live.com');
+        cy.get('#password').type('SaVLAq3&vX6F!ik&k8kiA^bAyLPN$@io');
+        cy.get('#password_confirmation').type('SaVLAq3&vX6F!ik&k8kiA^bAyLPN$@io');
+        cy.get('[data-test-id="register_button"]').click()
+        cy.url().should('be.equal', `${Cypress.config("baseUrl")}/dashboard`);
+        cy.contains('You\'re logged in!');
+    });
+
+    it('display message if email is already registered', () => {
+        cy.visit('/register');
+        cy.get('#name').type('Evandro Ribeiro');
+        cy.get('#email').type('ribeiroevandro@live.com');
+        cy.get('#password').type('SaVLAq3&vX6F!ik&k8kiA^bAyLPN$@io');
+        cy.get('#password_confirmation').type('SaVLAq3&vX6F!ik&k8kiA^bAyLPN$@io');
+        cy.get('[data-test-id="register_button"]').click()
+
+        cy.visit('/register');
+        cy.get('#name').type('Evandro Ribeiro');
+        cy.get('#email').type('ribeiroevandro@live.com');
+        cy.get('#password').type('SaVLAq3&vX6F!ik&k8kiA^bAyLPN$@io');
+        cy.get('#password_confirmation').type('SaVLAq3&vX6F!ik&k8kiA^bAyLPN$@io');
+        cy.get('[data-test-id="register_button"]').click()
+        // cy.url().should('be.equal', `${Cypress.config("baseUrl")}/dashboard`);
+        cy.contains('The email has already been taken.');
+    });
+});
