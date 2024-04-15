026e3bffb316b4612048e2e77d101441a85f4a03
Evandro Ribeiro <ribeiroevandro@live.com>
2024-04-15 01:06:15 -0300

    chore: updates

--- ci.config.js
+++ ci.config.js
@@ -12,7 +12,7 @@ module.exports = defineConfig({
         setupNodeEvents(on, config) {
             return require('./tests/cypress/plugins/index.js')(on, config)
         },
-        baseUrl: 'http://127.0.0.1:8000',
+        baseUrl: 'https://laravel-cypress.test',
         specPattern: 'tests/cypress/integration/**/*.spec.js',
         supportFile: 'tests/cypress/support/index.js',
         retries: 0
